6375eeffc900c0acfaa6757037fa9070be64368e
fergussontech <taylor.fergusson99@gmail.com>
2024-01-17 21:19:58 -0500

    adding hot house

---
+++ shop.php
@@ -0,0 +1,67 @@
+<?php $title = 'SHOP'; include("top.php");?>
+
+<main class="item-container">
+<div class="item">
+<a href="shop/dildo">
+<div class="shop-photo" id="item1"></div>
+</a>
+<h4>HOT HOUSE PINK DILDO</h4>
+<h4>&#8212;</h4>
+<h4><s>$250.00 CAD</s></h4>
+<button class="buy-button" id="sold-out">SOLD OUT</button>
+</div>
+
+<div class="item">
+<a href="shop/rosary">
+<div class="shop-photo" id="item2"></div>
+</a>
+<h4>HOT HOUSE PINK ROSARY</h4>
+<h4>&#8212;</h4>
+<h4><s>$30.00 CAD</s></h4>
+<button class="buy-button" id="sold-out">SOLD OUT</button>
+</div>
+
+<div class="item">
+<a href="shop/shirt">
+<div class="shop-photo" id="item3"></div>
+</a>
+<h4>I PARTIED IN THE HOT HOT HOUSE AND ALL I GOT WAS THIS LOUSY T-SHIRT SHIRT</h4>
+<h4>&#8212;</h4>
+<h4><s>$40.00 CAD</s></h4>
+<button class="buy-button" id="sold-out">SOLD OUT</button>
+</div>
+
+<div class="item">
+<a href="shop/vinyl">
+<div class="shop-photo" id="item4"></div>
+</a>
+<h4>PARTY JESUS / I'M EXPENSIVE VINYL</h4>
+<h4>&#8212;</h4>
+<h4><s>$40.00 CAD</s></h4>
+<button class="buy-button" id="sold-out">SOLD OUT</button>
+</div>
+
+<div class="item">
+<a href="shop/tape">
+<div class="shop-photo" id="item5"></div>
+</a>
+<h4>PARTY JESUS / I'M EXPENSIVE LIMITED EDITION PINK TAPE</h4>
+<h4>&#8212;</h4>
+<h4>$?</h4>
+<button class="buy-button" id="sold-out">COMING SOON</button>
+</div>
+
+<div class="item">
+<a href="shop/download">
+<div class="shop-photo" id="item6"></div>
+</a>
+<h4>PARTY JESUS / I'M EXPENSIVE DIGITAL DOWNLOAD</h4>
+<h4>&#8212;</h4>
+<h4>FREE</h4>
+<a href="https://girlsinthehothouse.bandcamp.com/album/party-jesus-im-expensive">
+<button class="buy-button">DOWNLOAD</button>
+</a>
+</div>
+</main>
+
+<?php include("bottom.php");?>
